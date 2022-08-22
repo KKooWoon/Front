e363d8965ae3b0cfbc16209150bbf4e2a02c05f9
amykim928 <hyeonah928@gmail.com>
2022-08-22 14:14:08 +0900

    feat: 내 레이스 페이지 퍼블리싱

--- src/components/my-race/my-race.style.ts
+++ src/components/my-race/my-race.style.ts
@@ -17,6 +17,46 @@ export const Wrapper = styled.div`
   }
 `;
 
+export const MyRaceWrapper = styled.div`
+  display: inline-block;
+  padding: 17px 12px 15px 12px;
+  width: 160px;
+  background-color: var(--purple-sub01);
+  border-radius: 20px;
+  position: relative;
+  & svg {
+    position: absolute;
+    transform: translate(126px, 0px);
+  }
+  & p {
+    font-size: 16px;
+    font-weight: 500;
+    color: #ac8eff;
+    text-align: left;
+  }
+  p + p + h3 {
+    padding-top: 67px;
+  }
+  p + h3 {
+    padding-top: 91px;
+  }
+  & h3 {
+    font-weight: 500;
+    font-size: 18px;
+    color: #6732ff;
+    padding-bottom: 5px;
+  }
+  & div {
+    display: inline-block;
+    padding: 5px 8px;
+    background-color: #ac8eff;
+    border-radius: 27px;
+    color: white;
+    font-size: 16px;
+    font-weight: 500;
+  }
+`;
+
 export const AddRaceBtn = styled.button`
   width: 160px;
   height: 209px;
