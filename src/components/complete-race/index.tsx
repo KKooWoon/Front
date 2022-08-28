efb52d775e805c5e6879a9856f05b5ec864de8ed
amykim928 <hyeonah928@gmail.com>
2022-08-29 06:20:41 +0900

    feat: 레이스 생성 페이지 백엔드 연동

--- src/components/complete-race/index.tsx
+++ src/components/complete-race/index.tsx
@@ -2,11 +2,18 @@ import React from 'react';
 import { Wrapper, ImageSection, InviteCodeWrapper } from './complete-race.style';
 import character from '@assets/images/character.png';
 
-const CompleteRace = () => {
+interface RaceProps {
+  raceResponse: {
+    raceCode: string;
+    racePassword: string;
+  };
+}
+
+const CompleteRace: React.FunctionComponent<RaceProps> = ({ raceResponse }) => {
   return (
     <Wrapper>
       <h1>
-        <span style={{ color: '#6732ff' }}>레이스 생성</span>이 완료되었습니다!
+        <span style={{ color: '#433f4b' }}>레이스 생성</span>이 완료되었습니다!
       </h1>
       <h1>레이스에 참여할 친구를 초대해보세요.</h1>
       <ImageSection>
@@ -14,10 +21,23 @@ const CompleteRace = () => {
           <img src={character} />
         </div>
       </ImageSection>
-      <InviteCodeWrapper>
-        <div>초대코드</div>
-        <h1>6ee4yajsldfj</h1>
-      </InviteCodeWrapper>
+      {raceResponse.racePassword ? (
+        <div>
+          <InviteCodeWrapper style={{ marginBottom: '0px' }}>
+            <div>초대코드</div>
+            <h1>{raceResponse.raceCode}</h1>
+          </InviteCodeWrapper>
+          <InviteCodeWrapper style={{ marginTop: '6px' }}>
+            <div>비밀번호</div>
+            <h1>{raceResponse.racePassword}</h1>
+          </InviteCodeWrapper>
+        </div>
+      ) : (
+        <InviteCodeWrapper>
+          <div>초대코드</div>
+          <h1>{raceResponse.raceCode}</h1>
+        </InviteCodeWrapper>
+      )}
     </Wrapper>
   );
 };
